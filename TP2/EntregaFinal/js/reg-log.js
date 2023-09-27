f30db5a3f01e92d06959a78c0e2ee9d121569dc8
Andres Buccella <andresbuccella@gmail.com>
2023-09-27 16:07:49 -0300

    login casi terminado

---
+++ TP2/EntregaFinal/js/reg-log.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelector(".a-iniciar-sesion").addEventListener("click", cambiarLoginReg);
+    document.querySelector(".a-registrarse").addEventListener("click", cambiarLoginReg);
+
+    function cambiarLoginReg(){
+        document.querySelector(".formulario-registro").classList.toggle("none");
+        document.querySelector(".formulario-login").classList.toggle("none");
+    }
+
+
+
+})
