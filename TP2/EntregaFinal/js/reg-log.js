cebfa186b27379ce6de3b474d6be7bf0397c963b
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2023-09-29 12:58:59 -0300

    Cambios login-register

--- TP2/EntregaFinal/js/reg-log.js
+++ TP2/EntregaFinal/js/reg-log.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector(".a-iniciar-sesion").addEventListener("click", cambiarLoginReg);
-    document.querySelector(".a-registrarse").addEventListener("click", cambiarLoginReg);
+    document.querySelector("#a-iniciar-sesion").addEventListener("click", cambiarLoginReg);
+    document.querySelector("#a-registrarse").addEventListener("click", cambiarLoginReg);
 
-    function cambiarLoginReg(){
-        document.querySelector(".formulario-registro").classList.toggle("none");
-        document.querySelector(".formulario-iniciar-sesion").classList.toggle("none");
+    function cambiarLoginReg() {
+        document.querySelector("#formulario-registro").classList.toggle("none");
+        document.querySelector("#formulario-iniciar-sesion").classList.toggle("none");
     }
-
-
 
 })
