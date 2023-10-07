9d290d8b5c463655591131238cad997072740801
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2023-10-07 05:04:02 -0300

    Caos registro

--- TP2/EntregaFinal/js/pagina-juego.js
+++ TP2/EntregaFinal/js/pagina-juego.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Estrellas
+ // Estrellas
     const estrellas = document.querySelectorAll('.estrellas .estrella');
 
     for (let i = 0; i < estrellas.length; i++) {
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
             for (let j = 0; j < estrellas.length; j++) {
                 estrellas[j].classList.remove('estrella-activa');
             }
-            if (estrellas_activas != i+1) {
+            if (estrellas_activas != i + 1) {
                 for (let j = 0; j <= i; j++) {
                     estrellas[j].classList.add('estrella-activa');
                 }
--- TP2/EntregaFinal/js/reg-log.js
+++ TP2/EntregaFinal/js/reg-log.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    //Cambio entre registro y login
     document.querySelector("#a-iniciar-sesion").addEventListener("click", cambiarLoginReg);
     document.querySelector("#a-registrarse").addEventListener("click", cambiarLoginReg);
 
@@ -9,20 +10,46 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
-    document.querySelector("#registrarse").addEventListener("click", confirmar_registro);
+    //CAOS
 
+    //Validacion de registro
+    let btn_registro = document.querySelector("#registrarse"); //Boton de registro
+    let status; //Variable resultado de validacion
+    btn_registro.addEventListener("click", validar_registro); //El boton chequea que se cumplan los campos
+
+    //Validacion individual de los campos del registro
     let form_regist = document.querySelector("#formulario-registro");
-    form_regist.querySelector("#user-name").addEventListener("input", function() {form_validate("user-name");});      
-    form_regist.querySelector("#nombre").addEventListener("input", function() {form_validate("nombre");});     
-    form_regist.querySelector("#apellido").addEventListener("input", function() {form_validate("apellido");});     
-    form_regist.querySelector("#edad").addEventListener("input", function() {form_validate("edad");});     
-    form_regist.querySelector("#correo").addEventListener("input", function() {form_validate("correo");});     
+    form_regist.querySelector("#user-name").addEventListener("input", function () { form_validate("user-name"); });
+    form_regist.querySelector("#nombre").addEventListener("input", function () { form_validate("nombre"); });
+    form_regist.querySelector("#apellido").addEventListener("input", function () { form_validate("apellido"); });
+    form_regist.querySelector("#edad").addEventListener("input", function () { form_validate("edad"); });
+    form_regist.querySelector("#correo").addEventListener("input", function () { form_validate("correo"); });
     form_regist.querySelector("#contraseña").addEventListener("input", validate_password);
     form_regist.querySelector("#rep-contraseña").addEventListener("input", confirm_password);
 
-    function confirmar_registro() {
+    const loader = document.querySelectorAll('.btn-registrarse .loader')[1]; //Loader del registro solo
 
-        let status = true;
+    //Resultado cuando la animacion del loader termina
+    loader.addEventListener('animationend', (event) => {
+        btn_registro.classList.remove("cheking");
+        if (status) {
+            btn_registro.classList.add("registro-ok");
+            btn_registro.addEventListener('animationend', (event) => {
+                window.location.href = "../index.html"; //Si el resultado de la validacion de ok te envia al index al terminar la animacion del boton registro
+            });
+        }
+        else btn_registro.classList.add("registro-not-ok");
+    });
+
+    //Funcion de validacion del boton de registro
+    function validar_registro() {
+        btn_registro.classList.remove("cheking");
+        btn_registro.classList.remove("registro-ok");
+        btn_registro.classList.remove("registro-not-ok");
+        loader.style.display = 'block';
+        btn_registro.classList.add("cheking");
+
+        status = true;
         if (!form_validate("user-name")) status = false;
         if (!form_validate("nombre")) status = false;
         if (!form_validate("apellido")) status = false;
@@ -30,10 +57,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!form_validate("correo")) status = false;
         if (!validate_password()) status = false;
         if (!confirm_password()) status = false;
-        
-        if (status) window.location.href = "../index.html"
     }
 
+    //Funcion de campo con datos
     function form_validate(id) {
         let input = document.querySelector("#" + id);
         let status = true;
@@ -49,6 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
         return status
     }
 
+    //Mensaje de error
     function form_error_message(errors, errors_status) {
         elem = '<div class="form-error"><img src="../images/form-error.png" alt="">'
 
@@ -71,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let errors_status = [];
 
         if (contraceña.value == "") errors_status.push(0); else errors_status.push(1);                                          //Falta completar el campo
-        if (contraceña.value.length >6) errors_status.push(1); else errors_status.push(0);                                      //La contraseña debe tener mas de 6 caracteres
+        if (contraceña.value.length > 6) errors_status.push(1); else errors_status.push(0);                                      //La contraseña debe tener mas de 6 caracteres
         if (/\d/.test(contraceña.value)) errors_status.push(1); else errors_status.push(0);                                     //Debe contener por lo menos 1 numero
         if (/[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/.test(contraceña.value)) errors_status.push(1); else errors_status.push(0);        //Debe contener por lo menos 1 simbolo especial
 
@@ -92,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let contraceña_rep = document.querySelector("#rep-contraseña");
 
         let status = true;
-        if (contraceña_rep.value != contraceña.value || contraceña_rep.value=="") {
+        if (contraceña_rep.value != contraceña.value || contraceña_rep.value == "") {
             status = false;
             contraceña_rep.classList.add("form-error-input");
             let lasterror = contraceña_rep.parentNode.querySelector(".form-error");
