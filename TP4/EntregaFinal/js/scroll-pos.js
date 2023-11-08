6bfa939baa64a002431422d348ad97f975f4876e
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2023-11-08 20:34:52 -0300

    a

--- TP4/EntregaFinal/js/scroll-pos.js
+++ TP4/EntregaFinal/js/scroll-pos.js
@@ -1,11 +1,28 @@
 let scrollPosition;
-
+getScrollPosition();
 function getScrollPosition() {
     return window.scrollY || window.pageYOffset;
 }
 
-// Ejemplo de cómo usar la función
 window.addEventListener('scroll', function () {
     scrollPosition = getScrollPosition();
     console.log('Posición de scroll:', scrollPosition);
+    nav(scrollPosition);
 });
+
+
+let navTitle = document.querySelector("#titulo");
+nav(scrollPosition);
+function nav(scroll) {
+    if (scroll==0) {
+        navTitle.style.position="";
+        navTitle.style.transform = ``
+    }
+    else if (scroll > 0 && scroll < 200) {
+        navTitle.style.position="fixed";
+        navTitle.style.transform = `translateY(${-210 * scroll / 200}px) scale(+${1 - 0.7 * scroll / 200})`
+    }else if (scroll>200){
+        navTitle.style.position="fixed";
+        navTitle.style.transform = `translateY(${-210}px) scale(+${0.3})`
+    }
+}
