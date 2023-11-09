2845bb12d93388d7a4b4052757482ea176505f96
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2023-11-09 00:33:35 -0300

    a

--- TP4/EntregaFinal/js/scroll-pos.js
+++ TP4/EntregaFinal/js/scroll-pos.js
@@ -10,22 +10,36 @@ window.addEventListener('scroll', function () {
     nav(scrollPosition);
 });
 
+window.addEventListener('resize', function () {
+    navWidth = navTitle.width;
+    scrollVarTitle = 300 * ( navTitle.height/301);
+    nav(scrollPosition);
+    getScrollPosition();
+});
+
 
 //Navegador
 let navTitle = document.querySelector("#titulo");
+let navWidth = navTitle.width;
+let scrollVarTitle = 300 * (navTitle.height/301);
+
 nav(scrollPosition);
 function nav(scroll) {
-    if (scroll==0) {
-        navTitle.style.position="";
-        navTitle.style.transform = ``
+    if (scroll == 0) {
+        navTitle.style.position = "";
+        navTitle.style.transform = "";
+        navTitle.style.width = "";
     }
-    else if (scroll > 0 && scroll < 200) {
-        navTitle.style.position="fixed";
-        navTitle.style.transform = `translateY(${-210 * scroll / 200}px) scale(+${1 - 0.7 * scroll / 200})`
-    }else if (scroll>200){
-        navTitle.style.position="fixed";
-        navTitle.style.transform = `translateY(${-210}px) scale(+${0.3})`
+    else if (scroll > 0 && scroll <= scrollVarTitle) {
+        navTitle.style.position = "fixed";
+        navTitle.style.transform = `translateY(${-100 * scroll / scrollVarTitle}px)`
+        navTitle.style.width = `${navWidth - ((navWidth - 175) * (scroll / scrollVarTitle))}px`
+    } else if (scroll > 300) {
+        navTitle.style.position = "fixed";
+        navTitle.style.transform = `translateY(-100px)`
+        navTitle.style.width = "175px"
     }
 }
 
 //Pesonajes 1
+//583->175
