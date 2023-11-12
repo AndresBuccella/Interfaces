49e3fe724f483262a3312d26e9d9b0c1acb24242
Andres Buccella <andresbuccella@gmail.com>
2023-11-12 19:24:34 -0300

    Segunda parte de la segunda parte

--- TP4/EntregaFinal/js/scroll-pos.js
+++ TP4/EntregaFinal/js/scroll-pos.js
@@ -18,18 +18,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener('resize', function () {
         navWidth = navTitle.width;
-        scrollVarTitle = 300 * (navTitle.height / 301);
+        scrollVarTitle = 300 * (navTitle.height / 301); //por que esta cuenta?
         scroll(getScrollPosition());
     });
 
     function scroll(scroll) {
         nav(scroll);
         edificios(scroll);
-        moverDuendeVerde(scroll)
+        moverDuendeVerde(scroll);
     }
 
     scroll(getScrollPosition());
 
+
+    /**
+     * Se encarga de controlar el tamanio del titulo, color del fondo del nav y tamanio del mismo con respecto al scroll realizado
+     * @param {number} scroll 
+     */
     function nav(scroll) {
         if (scroll == 0) {
             navegador.style.height = "";
@@ -38,30 +43,41 @@ document.addEventListener("DOMContentLoaded", function () {
             navTitle.style.position = "";
             navTitle.style.transform = "";
             navTitle.style.width = "";
+            //no veo diferencia con y sin este bloque, que cambia?
         }
         else if (scroll > 0 && scroll <= scrollVarTitle) {
             navegador.style.height = `${227 - 124 * scroll / scrollVarTitle}px`;
             navegador.style.zIndex = "9998";
-            navegador.style.background = "linear-gradient(180deg, rgba(84, 153, 248," + scroll / scrollVarTitle + ") 0%, rgba(84, 153, 248," + scroll / scrollVarTitle + ") 87.91%, rgba(84, 153, 248, 0) 100%)";
+
+            //transparencia del navegador. A medida que aumenta el color se hace más sólido
+            navegador.style.background = `linear-gradient(180deg, rgba(84, 153, 248,${scroll / scrollVarTitle}) 0%, rgba(84, 153, 248,${scroll / scrollVarTitle}) 87.91%, rgba(84, 153, 248, 0) 100%)`;
+            
             navTitle.style.position = "fixed";
-            navTitle.style.transform = `translateY(${-91 * scroll / scrollVarTitle}px)`
-            navTitle.style.width = `${navWidth - ((navWidth - 175) * (scroll / scrollVarTitle))}px`
+            //Transporta el titulo hacia arriba poco a poco al principio a cada vez más
+            // ya que scroll / scrollVarTitle va de 0 a casi 1
+            navTitle.style.transform = `translateY(${-91 * scroll / scrollVarTitle}px)`;
+            
+            //Cambia el ancho del titulo desde su ancho máximo navWidth hasta 175.
+            //Al principio no tiene reduccion ya que scroll y scrollVarTitle tienden a 0, pero a medida que crece (baja el scroll) 
+            //se va acercando a la diferencia entre el ancho del navegador y el ancho al que se desea llegar
+            navTitle.style.width = `${navWidth - ((navWidth - 175) * (scroll / scrollVarTitle))}px`;
         } else if (scroll > 300) {
+            //una vez que el scroll supera un determinado valor se setean valores fijos para que quede en el título
             navegador.style.height = "103px"
             navegador.style.zIndex = "9998";
             navegador.style.background = "";
             navTitle.style.position = "fixed";
-            navTitle.style.transform = `translateY(-91px)`
-            navTitle.style.width = "175px"
+            navTitle.style.transform = `translateY(-91px)`;
+            navTitle.style.width = "175px";
         }
     }
 
     //Edificios
 
     function edificios(scroll) {
-        let towerLeft = document.querySelector("#edificios-izquierda")
-        let towerCenter = document.querySelector("#edificios-centro")
-        let towerRight = document.querySelector("#edificios-derecha")
+        let towerLeft = document.querySelector("#edificios-izquierda");
+        let towerCenter = document.querySelector("#edificios-centro");
+        let towerRight = document.querySelector("#edificios-derecha");
         if (scroll == 0) {
             towerLeft.style.transform = ``;
             towerCenter.style.transform = ``;
@@ -87,19 +103,19 @@ document.addEventListener("DOMContentLoaded", function () {
     let milesSpiderWeb = divHeroes.querySelector("#tela-arania-derecha");
 
     divHeroes.addEventListener("mousemove", function mover(e) {
-
+                                                                                            //Aiura
         const offset1 = {
             x: (e.pageX - divHeroes.clientWidth / 2) / (divHeroes.clientWidth / 16),
             y: (e.pageY - divHeroes.clientHeight / 2) / (divHeroes.clientHeight / 16)
-        }
+        };
         const offset2 = {
             x: (e.pageX - divHeroes.clientWidth / 2) / (divHeroes.clientWidth / 8),
             y: (e.pageY - divHeroes.clientHeight / 2) / (divHeroes.clientHeight / 8)
-        }
+        };
         const offset3 = {
             x: (e.pageX - divHeroes.clientWidth / 2) / (divHeroes.clientWidth / 6),
             y: (e.pageY - divHeroes.clientHeight / 2) / (divHeroes.clientHeight / 6)
-        }
+        };
         peter.style.transform = `translate(calc(-50% + ${offset1.x}px), ${offset1.y}px)`;
         peterSpiderWeb.style.transform = `translate(${offset1.x}px, ${offset1.y}px)`;
 
@@ -114,10 +130,17 @@ document.addEventListener("DOMContentLoaded", function () {
     function moverDuendeVerde(scroll) {
         let duendeVerde = document.querySelector("#duende-verde");
         if (scroll > 800 && scroll < 1600) {
-            duendeVerde.style.transform = `translateY(${(scroll - 800) / 30}px) scaleX(-1)`
+            duendeVerde.style.transform = `translateY(${(scroll - 800) / 30}px) scaleX(-1)`;
+            console.log("duende");
         } else {
-            duendeVerde.style.transform = `translateY(${(1600 - 800) / 30}) scaleX(-1)`
+            duendeVerde.style.transform = `translateY(${(1600 - 800) / 30}) scaleX(-1)`;
         }
+        /* if (scroll > 150 && scroll < 1600) {
+            duendeVerde.style.transform = `translateY(${(scroll - 150) / 30}px) scaleX(-1)`;
+            console.log("duende");
+        } else {
+            duendeVerde.style.transform = `translateY(${(1600 - 150) / 30}) scaleX(-1)`;
+        } */
     }
 
 })
